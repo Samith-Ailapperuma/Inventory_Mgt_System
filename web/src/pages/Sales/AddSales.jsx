113eb72a0baca257315eacac3f9cb2c97aae34ae
Samith-Ailapperuma <samithailapperuma@gmail.com>
2026-03-09 02:33:06 +0530

    Added item choosing logic to the frontend

--- web/src/pages/Sales/AddSales.jsx
+++ web/src/pages/Sales/AddSales.jsx
@@ -1,8 +1,13 @@
-import { useEffect, useState, React } from "react";
+import { useEffect, useState, useMemo, React } from "react";
+import './sales.css';
 
 export default function AddSales() {
     const [items, setItems] = useState([]);
     const [selectedItem, setSelectedItem] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const selectedItemData = useMemo(() => {
+        return items.find(i => i.Item_Id === selectedItem);
+    }, [items, selectedItem]);
 
     useEffect(() => {
         fetch(`http://localhost:8080/getAllItems`)
@@ -12,19 +17,45 @@ export default function AddSales() {
     }, []);
 
     return (
-        <div>
+        <div className="add-sale-item">
             <h1>Add Sale</h1>
 
-            <label>
+            <label className="label">
                 Item:
-                <select 
+                <select
+                    className="select"
                     value={selectedItem}
                     onChange={(e) => setSelectedItem(e.target.value)}>
+                    <option value="" disabled>
+                        Select an item
+                    </option>
                     {items.map((item) => (
                         <option key={item.Item_Id} value={item.Item_Id}>{item.Item_Name}</option>
                     ))}
                 </select>
             </label>
+
+            <label class="label">
+                Amount/No.of Units:
+                <input
+                    type="number"
+                    min="1"
+                    className="select"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                    placeholder="Enter quantity"
+                />
+            </label>
+
+            <label class="label">
+                Price:
+                <div className="select">{selectedItemData?.Unit_Price}
+                </div>
+            </label>
+
+            <button>Add item</button>
+
+            <button>Complete Sale</button>
         </div>
     );
 
